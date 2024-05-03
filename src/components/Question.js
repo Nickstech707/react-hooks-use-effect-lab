305e7e0efda177ab70fc010154a9946f46cf7bb7
Nicholas Muriithi <nickstech707@gmail.com>
2024-05-03 12:17:51 +0300

    Uses the useEffect hook with the dependencies array and a cleanup function with useEffect to stop background processes

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    let timer;
+
+    // Function to decrement the time remaining by 1 every second
+    const countdown = () => {
+      setTimeRemaining((prevTime) => {
+        if (prevTime > 0) {
+          return prevTime - 1;
+        } else {
+          setTimeRemaining(10);
+          onAnswered(false);
+          clearInterval(timer);
+          return prevTime;
+        }
+      });
+    };
+
+    // Start the timer using setInterval
+    timer = setInterval(countdown, 1000);
+
+    // Cleanup function to stop the timer when the onAnswered prop changes
+    return () => {
+      clearInterval(timer);
+    };
+  }, [onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
